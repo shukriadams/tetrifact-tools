5a2acc0b04ae77d9ab1aef77a9632ee571af4103
Shukri Adams <shukri.adams@gmail.com>
2020-12-12 23:08:00 +0100

    added quota deletes

--- src/lib/getArchive.js
+++ src/lib/getArchive.js
@@ -1,4 +1,5 @@
 const process = require('process'), 
+    path = require('path'),
     fs = require('fs-extra'),
     minimist = require('minimist'),
     urljoin = require('urljoin'),
@@ -8,11 +9,17 @@ const process = require('process'),
 module.exports = async function(){
     const argv = minimist(process.argv.slice(2)),
         host = argv.host,
-        maxPackages = argv.host || 10,
+        maxPackages = argv.maxPackages || 2,
+        store = argv.store,
         package = argv.package
 
     if (!host){
         console.error('ERROR : host not defined. Use --host arg')
+        return process.exit(1)
+    }
+
+    if (!store){
+        console.error('ERROR : store not defined. Use --store arg')
         return process.exit(1)
     }
 
@@ -27,16 +34,20 @@ module.exports = async function(){
         return process.exit(1)
     }
 
+    if (!await fs.exists(store))
+        await fs.ensureDir(store)
+
     const remoteURL = urljoin(host, 'v1/archives/', package),
-        savePath = 'dl.zip',
-        extractPath = 'dl',
-        extractedFlag = '.dl'
+        savePath = path.join(store, `~${package}` ),
+        extractPath = path.join(store, package),
+        extractedFlag = path.join(store, `.${package}`)
 
     // check if the package has already been downloaded, we don't use the unpack folder presence for this as the folder
     // can be created but still be in an error state. Use the post-unpack flag instead
     if (!await fs.exists(extractedFlag)){
         // ensure package exists
         const status = await httputils.getStatus(remoteURL)
+        
         if (status === 404){
             console.error(`ERROR : package ${remoteURL} does not exist`)
             return process.exit(1)
@@ -44,7 +55,6 @@ module.exports = async function(){
 
         try {
             await httputils.downloadFile(remoteURL, savePath)
-
         } catch(ex){
             console.error(`ERROR : ${ex}`)
             return process.exit(1)
@@ -52,6 +62,7 @@ module.exports = async function(){
 
         // check if the downloaded file is empty, this is often caused by specifying the wrong protocol
         const stats = fs.statSync(savePath)
+        
         if (!stats.size)
             console.log(`WARNING : the package from ${remoteURL} is empty. This can often happen when the wrong host protocol (http/https) is used.`)
 
@@ -63,8 +74,45 @@ module.exports = async function(){
             return process.exit(1)
         }
 
+        await fs.remove(savePath)
+
         // write done flag
-        await fs.outputFile(extractedFlag, '')
+        await fs.writeJson(extractedFlag, { created : new Date().getTime() })
+    }
+
+
+
+    // remove older packages that exceed quota
+    let downloadedPackagesFlags = await fsUtils.readFilesInDir(store, false),
+        downloadedPackages = []
+
+    for (const packageFlag of downloadedPackagesFlags){
+        if (!packageFlag.startsWith('.'))
+            continue
+
+        let packageFlagContent
+        try {
+            packageFlagContent = await fs.readJson(path.join(store, packageFlag))
+        } catch (ex){
+            // file is not a flag, ignore
+            continue
+        }
+
+        downloadedPackages.push({ created : packageFlagContent.created, package : packageFlag })
+    }
+    
+    downloadedPackages = downloadedPackages.sort((a, b)=> {
+        return a.created > b.created ? 1 :
+            b.created > a.created ? -1 :
+            0
+    })
+
+    if (downloadedPackages.length > maxPackages){
+        downloadedPackages = downloadedPackages.map(r => r.package).slice(0, downloadedPackages.length - maxPackages)
+        for (const downloadedPackage of downloadedPackages){
+            await fs.remove(path.join(store, downloadedPackage))
+            await fs.remove(path.join(store, downloadedPackage.substr(1)))
+        }
     }
 
     console.log(extractPath)
