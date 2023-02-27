425451219e6c038e249452ff1c6f06f042e15634
Shukri Adams <shukri.adams@gmail.com>
2023-02-27 15:35:08 +0100

    improved settings handling

--- src/lib/settings.js
+++ src/lib/settings.js
@@ -1,4 +1,6 @@
 let fs = require('fs-extra'),
+    path = require('path'),
+    process = require('process'),
     yaml = require('js-yaml'),
     settings = {
         
@@ -9,12 +11,23 @@ let fs = require('fs-extra'),
         store : null
     }
 
- // Load settings from YML file, merge with default settings
-if (fs.existsSync('./tetrifact.yml')){
+// Load settings from YML file, merge with default settings
+
+// try to find config path, if running from pkg compiled exe it will be in dir relative 1st process.argv val, if running from 
+// node source files it will relative to 2nd value
+let settingsPath = path.join(path.dirname(process.argv[0]), 'tetrifact.yml')
+if (!fs.existsSync(settingsPath)){
+    settingsPath = path.join(path.dirname(process.argv[1]), 'tetrifact.yml')
+    if (!fs.existsSync(settingsPath))
+        settingsPath = null
+}
+
+if (settingsPath){
+    console.log(`Found settings file at ${settingsPath}`)
     let userSettings = null
 
     try {
-        const settingsYML = fs.readFileSync('./tetrifact.yml', 'utf8')
+        const settingsYML = fs.readFileSync(settingsPath, 'utf8')
         userSettings = yaml.safeLoad(settingsYML)
     } catch (e) {
         console.error('Error reading settings.yml', e)
