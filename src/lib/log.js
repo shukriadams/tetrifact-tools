0062cd20df607e02573b1b1773afc25350dc5af5
Shukri Adams <shukri.adams@gmail.com>
2024-02-23 14:46:53 +0100

    setup

--- src/lib/log.js
+++
@@ -1,5 +0,0 @@
-let winstonWrapper = require('winston-wrapper'),
-    settings = require('./settings').get(),
-    log = winstonWrapper.new(settings.logDir, settings.logLevel).log
-
-module.exports = log
--- src/lib/urlHelper.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    ensureFormat(url){
-        if (!url.toLowerCase().startsWith('http://') && !url.toLowerCase().startsWith('https://'))
-            url = `http://${url}`
-
-        return url
-    }
-}
